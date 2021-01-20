fff1f0481c9e5543aaab965b0a1bbd6e3c8f342d
Dhanuka99 <danuka1999lakshan@gmail.com>
2021-01-21 00:03:22 +0530

    example

--- src/lk/ijse/hibernate/Appinitializer.java
+++ src/lk/ijse/hibernate/Appinitializer.java
@@ -1,12 +1,24 @@
 package lk.ijse.hibernate;
 
+import entity.Student;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.hibernate.Session;
+import org.omg.CORBA.UserException;
+import util.FactoryConfiguration;
 
 public class Appinitializer {
 
     public static void main(String[] args){
+        Student student = new Student();
+        student.setId("S001");
+        student.setName("Dhanuka");
+        student.setAddress("Kurunegala");
 
+        Session session = FactoryConfiguration.getInstance().getSession();
+        session.save(student);
+
+        session.close();
     }
 
 
--- src/util/FactoryConfiguration.java
+++ src/util/FactoryConfiguration.java
@@ -2,12 +2,15 @@ package util;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class FactoryConfiguration {
     private static FactoryConfiguration factoryConfiguration;
     private SessionFactory sessionFactory;
 
     private FactoryConfiguration(){
+        Configuration configuration = new Configuration().configure();
+        sessionFactory = configuration.buildSessionFactory();
 
     }
 
